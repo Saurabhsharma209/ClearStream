1b0a7f0b1a5f63f72883678f957054381225f0ef
ClearStream Dev Agent <saurabh.sharma@exotel.com>
2026-06-03 10:11:11 +0530

    [QA] generate testdata WAV fixtures for SNR benchmark

--- testdata/generate_noisy.go
+++ testdata/generate_noisy.go
@@ -9,6 +9,33 @@ import (
 	"os"
 )
 
+func writeWAVHeader(f *os.File, numSamples int) {
+	dataSize := uint32(numSamples * 2)
+	f.Write([]byte("RIFF"))
+	binary.Write(f, binary.LittleEndian, uint32(36+dataSize))
+	f.Write([]byte("WAVE"))
+	f.Write([]byte("fmt "))
+	binary.Write(f, binary.LittleEndian, uint32(16))
+	binary.Write(f, binary.LittleEndian, uint16(1))     // PCM
+	binary.Write(f, binary.LittleEndian, uint16(1))     // mono
+	binary.Write(f, binary.LittleEndian, uint32(16000)) // sample rate
+	binary.Write(f, binary.LittleEndian, uint32(32000)) // byte rate
+	binary.Write(f, binary.LittleEndian, uint16(2))     // block align
+	binary.Write(f, binary.LittleEndian, uint16(16))    // bits per sample
+	f.Write([]byte("data"))
+	binary.Write(f, binary.LittleEndian, dataSize)
+}
+
+func clamp16(v float64) int16 {
+	if v > 32767 {
+		return 32767
+	}
+	if v < -32768 {
+		return -32768
+	}
+	return int16(v)
+}
+
 func main() {
 	sampleRate := 16000
 	duration := 5
@@ -18,39 +45,61 @@ func main() {
 		panic(err)
 	}
 
-	f, err := os.Create("testdata/sample_noisy.wav")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	dataSize := uint32(numSamples * 2)
-
-	// RIFF header
-	f.Write([]byte("RIFF"))
-	binary.Write(f, binary.LittleEndian, uint32(36+dataSize))
-	f.Write([]byte("WAVE"))
-
-	// fmt chunk
-	f.Write([]byte("fmt "))
-	binary.Write(f, binary.LittleEndian, uint32(16))
-	binary.Write(f, binary.LittleEndian, uint16(1))     // PCM
-	binary.Write(f, binary.LittleEndian, uint16(1))     // mono
-	binary.Write(f, binary.LittleEndian, uint32(16000)) // sample rate
-	binary.Write(f, binary.LittleEndian, uint32(32000)) // byte rate
-	binary.Write(f, binary.LittleEndian, uint16(2))     // block align
-	binary.Write(f, binary.LittleEndian, uint16(16))    // bits per sample
-
-	// data chunk
-	f.Write([]byte("data"))
-	binary.Write(f, binary.LittleEndian, dataSize)
-
 	rng := rand.New(rand.NewSource(42))
-	for i := 0; i < numSamples; i++ {
-		t := float64(i) / float64(sampleRate)
-		speech := math.Sin(2*math.Pi*440*t) * 16000.0
-		noise := (rng.Float64()*2 - 1) * 3200.0 // ~20% of 16000
-		sample := int16(speech + noise)
-		binary.Write(f, binary.LittleEndian, sample)
+
+	// --- sample_clean.wav: pure 440Hz sine wave ---
+	{
+		f, err := os.Create("testdata/sample_clean.wav")
+		if err != nil {
+			panic(err)
+		}
+		writeWAVHeader(f, numSamples)
+		for i := 0; i < numSamples; i++ {
+			t := float64(i) / float64(sampleRate)
+			sample := math.Sin(2*math.Pi*440*t) * 28000.0
+			binary.Write(f, binary.LittleEndian, clamp16(sample))
+		}
+		f.Close()
+	}
+
+	// --- sample_noisy.wav: 440Hz sine + white noise, SNR ~10dB ---
+	// SNR 10dB => noise amplitude = signal_amplitude / sqrt(10) ~ signal / 3.16
+	{
+		f, err := os.Create("testdata/sample_noisy.wav")
+		if err != nil {
+			panic(err)
+		}
+		writeWAVHeader(f, numSamples)
+		sigAmp := 16000.0
+		noiseAmp := sigAmp / math.Sqrt(10) // ~10dB SNR
+		for i := 0; i < numSamples; i++ {
+			t := float64(i) / float64(sampleRate)
+			speech := math.Sin(2*math.Pi*440*t) * sigAmp
+			noise := (rng.Float64()*2 - 1) * noiseAmp
+			binary.Write(f, binary.LittleEndian, clamp16(speech+noise))
+		}
+		f.Close()
+	}
+
+	// --- sample_office.wav: 440Hz sine + pink-ish (low-freq biased) noise ---
+	{
+		f, err := os.Create("testdata/sample_office.wav")
+		if err != nil {
+			panic(err)
+		}
+		writeWAVHeader(f, numSamples)
+		sigAmp := 16000.0
+		noiseAmp := sigAmp / math.Sqrt(10)
+		var smoothed float64
+		for i := 0; i < numSamples; i++ {
+			t := float64(i) / float64(sampleRate)
+			speech := math.Sin(2*math.Pi*440*t) * sigAmp
+			white := (rng.Float64()*2 - 1)
+			// Low-freq bias: 70% smoothed (prev) + 30% new white
+			smoothed = smoothed*0.7 + white*0.3
+			noise := smoothed * noiseAmp
+			binary.Write(f, binary.LittleEndian, clamp16(speech+noise))
+		}
+		f.Close()
 	}
 }
